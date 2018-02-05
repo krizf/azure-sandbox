4c79901ec0eb9cae90db259f1c71435a2173a58f
krizf <fanda.kriz@gmail.com>
2018-02-05 10:41:13 +0000

    adding terraform installation as a shell script

---
+++ install/terraform.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install unzip
+cd /tmp
+wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip?_ga=2.229820956.1065078487.1517821732-1082545299.1507618762
+mv terraform_0.11.3_linux_amd64.zip?_ga=2.229820956.1065078487.1517821732-1082545299.1507618762 terraform_0.11.3_linux_amd64.zip
+unzip terraform_0.11.3_linux_amd64.zip
+sudo mv terraform /usr/local/bin
+terraform --version
